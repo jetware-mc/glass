73c4c5071124f4f15b38b3c9bef06191069b0380
theDevJade <jadeisstolen@gmail.com>
2023-10-22 21:03:51 -0700

    fixed main class

---
+++ subscape-main/src/main/kotlin/com/autumnstudios/plugins/subscape/Subscape.kt
@@ -0,0 +1,13 @@
+package com.autumnstudios.plugins.subscape
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Subscape : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
