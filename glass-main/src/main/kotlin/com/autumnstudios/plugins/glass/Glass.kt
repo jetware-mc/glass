28f20437754c6e38ba63180a7ea9bdf5bc8da6ee
theDevJade <jadeisstolen@gmail.com>
2023-11-07 12:58:08 -0800

    Basic Relay Server

--- glass-main/src/main/kotlin/com/autumnstudios/plugins/glass/Glass.kt
+++ glass-main/src/main/kotlin/com/autumnstudios/plugins/glass/Glass.kt
@@ -1,31 +1,40 @@
 package com.autumnstudios.plugins.glass
 
 import com.autumnstudios.plugins.glass.plugins.startup.GlassPluginRuntime
+import com.autumnstudios.plugins.glass.tcp.RelayServer
 import org.bukkit.plugin.java.JavaPlugin
 
-class Glass(launcher: JavaPlugin) {
+class Glass() : JavaPlugin() {
 
 
+    override fun onEnable() {
+        instance = this; i = this
 
-    // Singleton instance for easier access
+        RelayServer.create()
+
+        startGlassPlugins()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+
     companion object {
         lateinit var instance: JavaPlugin
         lateinit var i: JavaPlugin
 
         init {
-            System.loadLibrary("native")
+            print("Attempting to load native library.")
+            try {
+                System.loadLibrary("native")
+            } catch (e: Exception) {
+                print("Failed")
+                print(e.message)
+
+            }
         }
     }
-
-    init {
-        instance = launcher; i = launcher
-    }
-
-    fun main(args : Array<String>) {
-
-    }
-
-
 
     // Start the plugins managed by GlassPluginRuntime
     private fun startGlassPlugins() {
--- glass-main/src/main/kotlin/com/autumnstudios/plugins/glass/lang/rust/GlobalRustPluginData.kt
+++
@@ -1,6 +0,0 @@
-package com.autumnstudios.plugins.glass.lang.rust
-
-object GlobalRustPluginData {
-
-    val registeredPlugins : MutableList<String> = ArrayList()
-}
