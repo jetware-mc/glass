97a3e825d4f56a20949386dca05025597deea7b1
theDevJade <jadeisstolen@gmail.com>
2023-11-12 10:38:08 -0800

    Added spigot libs

---
+++ binaries/rust/rust_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+
+export function run(): void;
+
+export function print_string(a: number, b: number): void;
+
+export function print_str(a: number, b: number): void;
+
+export function print_int(a: number): void;
+
+export function print_long(a: number): void;
+
+export function print_short(a: number): void;
+
+export function print_float(a: number): void;
+
+export function print_double(a: number): void;
+
+export function __wbindgen_malloc(a: number, b: number): number;
+
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
