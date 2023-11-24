8abf33e673489ba274da8f18955813c0371c5a2b
thedevjade <jadeisstolen@gmail.com>
2023-11-24 09:16:29 -0800

    Wowee

--- glass-kotlin/build.gradle.kts
+++ glass-kotlin/build.gradle.kts
@@ -71,10 +71,10 @@ tasks {
     processResources {
         filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
         val props = mapOf(
-            "name" to project.name,
-            "version" to project.version,
-            "description" to project.description,
-            "apiVersion" to "1.20"
+                "name" to project.name,
+                "version" to project.version,
+                "description" to project.description,
+                "apiVersion" to "1.20"
         )
         inputs.properties(props)
         duplicatesStrategy = DuplicatesStrategy.INCLUDE
